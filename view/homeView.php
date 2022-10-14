0a9dad9038f22d9b178bf4bc6ba71f8291d87bb6
Fernando Icardi <fernando.icardi@gmail.com>
2022-10-13 23:48:33 -0300

    - se ajusto la rutas para que funcionen [controller]/[metodo] se arreglo linea 278 mustache parse para que no muestre el warning en pantalla - password con type ="password"

--- view/homeView.php
+++ view/homeView.php
@@ -1,7 +1,8 @@
 {{> header}}
 <div class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px" id="band">
     {{#usuario}}
-        <h2 class="w3-wide">{{nombre}}</h2>
+        <h2 class="w3-wide">Bienvenido {{Nombre}}</h2>
+        <p> {{Email}} - {{IdTipoUsuario}}</p>
     {{/usuario}}
     {{^usuario}}
         el usuario ingreso no existe
