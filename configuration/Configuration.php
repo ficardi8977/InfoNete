ef2e8876d73b41ad3d5f36ca3276af500aee4b4f
Fernando Icardi <fernando.icardi@gmail.com>
2022-10-25 00:49:41 -0300

    se separa productos de login, uso de redirect y clase sessionData

--- configuration/Configuration.php
+++ configuration/Configuration.php
@@ -4,6 +4,7 @@ include_once('helpers/MySQlDatabase.php');
 include_once('helpers/MustacheRenderer.php');
 include_once('helpers/Logger.php');
 include_once('helpers/Router.php');
+include_once('helpers/SesionData.php');
 
 
 
@@ -20,22 +21,23 @@ include_once ('dependencies/mustache/src/Mustache/Autoloader.php');
 class Configuration {
     private $database;
     private $view;
+    private $sesion;
 
     public function __construct() {
         $this->database = new MySQlDatabase();
         $this->view = new MustacheRenderer("view/", 'view/partial/');
+        $this->sesion = new SesionData(new Logger());
     }
 
     // CONFIGS DE CONTROLLER //
 
     public function getHomeController(){
-        return new HomeController($this->createUsuarioModel(),$this->createProductoModel(), $this->view);
+        return new HomeController($this->createProductoModel(), $this->view, $this->sesion);
     }
 
     public function getUsuarioController(){
-        return new UsuarioController($this->createUsuarioModel(), $this->view);
+        return new UsuarioController($this->createUsuarioModel(), $this->view, $this->sesion, $this->getRouter());
     }
-
     // //
     // CONFIGS DE MODEL //
 
@@ -49,6 +51,4 @@ class Configuration {
     public function getRouter() {
         return new Router($this, "home", "get");
     }
-
-    //
 }
--- controller/UsuarioController.php
+++ controller/UsuarioController.php
@@ -3,16 +3,17 @@
 class UsuarioController {
     private $usuarioModel;
     private $render;
+    private $sesion;
 
-    public function __construct($usuarioModel, $render){
+    public function __construct($usuarioModel, $render,  $sesion){
         $this->usuarioModel = $usuarioModel;
         $this->render = $render;
+        $this->sesion = $sesion;
     }
   
     public function alta()
     {
-        $data["session"] = $_SESSION;
-        echo $this->render->render("signinView.mustache", $data);
+        echo $this->render->render("signinView.mustache", $this->sesion->cargar());
     }
 
     public function verificar()
@@ -25,8 +26,30 @@ class UsuarioController {
 
         $this->usuarioModel->addUsuario($nombre, $password,$email,$coordenadasX,$coordenadasY);
 
-        // llamar a metodo que envia mail con codigo
         echo $this->render->render("verificacionUsuarioView.mustache");
+    }
+
+    public function login()
+    {
+        $nombre = $_POST["nombre"];
+        $password = $_POST["password"];
+
+        $result = $this->usuarioModel->getUsuario($nombre, $password); 
+        
+        if(isset($result[0]))
+        {
+            $this->sesion->guardar($result[0]["Nombre"], $result[0]["IdTipoUsuario"], true);
+        }else
+        {
+            $this->sesion->logueado(false);
+        }
+        Redirect::doIt();
+    }
+
+    public function close()
+    {
+        session_destroy();
+        Redirect::doIt();
     }
 
 }
