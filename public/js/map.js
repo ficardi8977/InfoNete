12054bf828f0210dc09ae579cf3ac4da646b7337
Fernando Icardi <fernando.icardi@gmail.com>
2022-10-29 14:46:39 -0300

    mapa

---
+++ public/js/map.js
@@ -0,0 +1,7 @@
+var mapOptions = {
+    center:new google.maps.LatLng(-34.6686986,-58.5614947),
+    zoom:12,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+};
+
+var map = new google.maps.Map(document.getElementById("mapa"),mapOptions);
