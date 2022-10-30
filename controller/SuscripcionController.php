a3386204261f26cd48351d593a54f5516c419d4d
evelinanunez <evelinacelestenunez@gmail.com>
2022-10-30 16:13:14 -0300

    baja suscripcion

--- controller/SuscripcionController.php
+++ controller/SuscripcionController.php
@@ -14,17 +14,23 @@ class SuscripcionController{
     public function alta(){
              $idProducto=$_POST["Id"];  
              $this->suscripcion->alta($idProducto);
-            $data= $this->mostrarSuscripciones();
+            $data['suscripciones']= $this->mostrarSuscripciones();
             echo $this->render->render("suscripcionesView.mustache", $this->sesion->cargar($data));
 
     }
 
     public function mostrarSuscripciones(){
              return $data['suscripciones']= $this->suscripcion->listSuscripciones();
+
   
     }
 
-
+    public function baja(){
+             $idProducto=$_POST["IdProducto"];   
+             $this->suscripcion->baja($idProducto);
+             $data['suscripciones']= $this->mostrarSuscripciones();
+             echo $this->render->render("suscripcionesView.mustache", $this->sesion->cargar($data));
+    }
 
 }
 
