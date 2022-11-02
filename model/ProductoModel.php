5f2e35143fa48655f9d19dd1eece14c4564229bc
Fernando Icardi <fernando.icardi@gmail.com>
2022-11-01 23:46:52 -0300

    arreglo de commit

---
+++ model/ProductoModel.php
@@ -0,0 +1,16 @@
+<?php
+
+class ProductoModel
+{
+    private $database;
+
+    public function __construct($database)
+    {
+        $this->database = $database;
+    }
+    public function getproductos()
+    {
+        return $this->database->query("SELECT * FROM producto");
+    }
+    
+}
