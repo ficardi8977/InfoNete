234ef0281b45e0f62438e67bf3ab775b0fda27b6
tomasPalavecino <tpalavecino@alumno.unlam.edu.ar>
2022-11-13 22:13:22 -0300

    controller-method

--- index.php
+++ index.php
@@ -5,4 +5,7 @@ include_once ("configuration/Configuration.php");
 $configuration = new Configuration();
 $router = $configuration->getRouter();
 
-$router->redirect($_GET['controller'],$_GET['method']);
+$controller = isset($_GET['controller']) ? $_GET['controller'] : '';
+$method = isset($_GET['method']) ? $_GET['method'] : '';
+
+$router->redirect($controller,$method);
